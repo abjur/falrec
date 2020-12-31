1340bb9185d82fd08d4bb21f7f98446254a3c8e6
Julio Trecenti <julio.trecenti@gmail.com>
2020-12-31 16:18:19 -0300

    create directory explicitly

--- data-raw/update.R
+++ data-raw/update.R
@@ -1,5 +1,6 @@
 ## code to prepare `falrec` dataset goes here
 
 falrec <- falrec::load_falrec()
+dir.create("inst/extdata", FALSE, TRUE)
 writexl::write_xlsx(falrec, "inst/extdata/falrec.xlsx")
 usethis::use_data(falrec, overwrite = TRUE)
