3fa7dfaf7bf2c9cb2ba658282e31b2fef5482e69
Katerine Linda Witkoski <ka.lwitkoski@gmail.com>
2021-11-12 12:43:41 -0300

    arruma diretório

--- R/load.R
+++ R/load.R
@@ -27,8 +27,8 @@ load_falrec <- function() {
     paste0("remove_", 1:3)
   )
 
-  unzip <- unzip(f, exdir = here::here('data-raw/zips'))
-  path <- ("~/Documents/falrec/data-raw/zips/FACONS (13).xls")
+  unzip <- unzip(f, exdir = here::here('data-raw'))
+  path <- ("~/Documents/falrec/data-raw/FACONS (13).xls")
 
   da_falrec <- readxl::read_excel(path, skip = 5, col_names = colunas)
   da_falrec <- da_falrec[, !grepl("remove", colunas)]
