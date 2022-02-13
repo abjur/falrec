fdd1185f6746f5bdbf2f81a415bedb2727357343
jtrecenti <julio.trecenti@gmail.com>
2022-02-13 20:01:22 -0300

    update readme and load all

--- data-raw/update.R
+++ data-raw/update.R
@@ -1,5 +1,6 @@
 ## code to prepare `falrec` dataset goes here
 
+devtools::load_all()
 falrec <- falrec::load_falrec()
 dir.create("inst/extdata", FALSE, TRUE)
 writexl::write_xlsx(falrec, "inst/extdata/falrec.xlsx")
